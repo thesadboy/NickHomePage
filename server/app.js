b0934385935d7c1bbc418dc19975ee6fa7865415
Nick Zhang <thesadboy@qq.com>
2020-02-13 18:10:41 +0800

    update

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const onerror = require('koa-onerror');
+const path = require('path');
+
+const port = 81;
+
+// error handler
+onerror(app);
+
+// middlewares
+app.use(require('koa-static')(path.join(__dirname, '../dist')));
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+app.on('error', function (err, ctx) {
+  console.error('server error', err, ctx);
+});
+
+module.exports = app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
