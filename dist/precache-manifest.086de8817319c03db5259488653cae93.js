185fd1046a0986fb3e8ff4410e1794e8f6086233
Nick Zhang <thesadboy@qq.com>
2020-02-13 18:17:05 +0800

    update

---
+++ dist/precache-manifest.086de8817319c03db5259488653cae93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "79a40cbf41060002fe9a",
+    "url": "/js/chunk-vendors.63c2fe0a.js"
+  },
+  {
+    "revision": "a5095b149963fef382cb",
+    "url": "/js/app.bccd5cef.js"
+  },
+  {
+    "revision": "9955e3b88598142bd65ca2e5ce1041c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79a40cbf41060002fe9a",
+    "url": "/css/chunk-vendors.ce4e137c.css"
+  },
+  {
+    "revision": "a5095b149963fef382cb",
+    "url": "/css/app.61dbf3a6.css"
+  }
+];
